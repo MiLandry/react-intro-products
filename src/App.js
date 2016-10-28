e18fa10b7b80cd842239624a37ff36ebc906110d
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-10-28 15:16:07 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Product from './Product';
+import './App.css';
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <Product />
+      );
+  },
+});
+
+export default App;
